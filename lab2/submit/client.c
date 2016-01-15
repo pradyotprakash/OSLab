304385e235b9296b4570dfceab8e19a9caf5adf7
Pradyot Prakash <pp2105@gmail.com>
2016-01-15 21:36:35 +0530

    time sharing left

--- lab2/submit/client.c
+++ lab2/submit/client.c
@@ -6,6 +6,8 @@
 #include <string.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
 
 #define PACKETSIZE 512
 
@@ -16,59 +18,86 @@ void error(char *msg){
 
 struct sockaddr_in serv_addr;
 struct hostent *server;
-int sockfd, portno;
+int portno, isRandom = 0;
+float duration, think_time;
 
 void *_main(void* arg){
-	int n;
-	
-	/* create socket, get sockfd handle */
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) 
-		error("ERROR opening socket");
+	while(1){
+		int n, sockfd;
 		
-	/* connect to server */
-	if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
-		error("ERROR connecting");
+		/* create socket, get sockfd handle */
+		sockfd = socket(AF_INET, SOCK_STREAM, 0);
+		if (sockfd < 0)
+			error("ERROR opening socket");
+			
+		/* connect to server */
+		if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+			error("ERROR connecting");
 
-	char *buffer = "get client.c";
+		char *buffer = (char *) malloc(30);
+		if(isRandom){
+			int r = rand()%10;
+			sprintf(buffer, "get files/foo%d.txt", r);
+		}
+		else{
+			sprintf(buffer, "get files/foo0.txt");
+		}
 
-	/* send user message to server */
-	n = write(sockfd, buffer, strlen(buffer));
-	if (n < 0)
-		error("ERROR writing to socket");
+		/* send user message to server */
+		n = write(sockfd, buffer, strlen(buffer));
+		if (n < 0)
+			error("ERROR writing to socket");
 
-	/* read reply from server */
-	char recvbuffer[PACKETSIZE];
-	int bytesRead;
-	int totalBytes = 0;
+		/* read reply from server */
+		char recvbuffer[PACKETSIZE];
+		int bytesRead;
+		int totalBytes = 0;
 
-	while((bytesRead = read(sockfd, recvbuffer, PACKETSIZE)) > 0){
-		totalBytes += bytesRead; 
+		while((bytesRead = read(sockfd, recvbuffer, PACKETSIZE)) > 0){
+			totalBytes += bytesRead; 
+		}
+
+		if (bytesRead < 0)
+			error("ERROR reading from socket");
+		
+		if(totalBytes > 0 && totalBytes < PACKETSIZE)
+			printf("%s\n", recvbuffer);
+		else if(totalBytes > 0)
+			printf("File Successfully received\n");
+		else if(totalBytes == 0){
+			printf("0 bytes received\n");
+		}
+		
+		close(sockfd);
+		free(buffer);
+		break;
+		usleep(think_time);
 	}
-
-	if (bytesRead < 0)
-		error("ERROR reading from socket");
-	
-	if(totalBytes > 0 && totalBytes < PACKETSIZE)
-		printf("%s\n", recvbuffer);
-	else if(totalBytes > 0)
-		printf("File Successfully received\n");
-	else if(totalBytes == 0){
-		printf("0 bytes received\n");
-	}
-	
-	close(sockfd);
-	return;
 }
 
 int main(int argc, char *argv[]){
 	
-	if (argc < 3) {
-		fprintf(stderr,"usage %s hostname port\n", argv[0]);
+	int NUM_THREADS = 1;
+
+	if (argc < 7) {
+		fprintf(stderr, "usage %s hostname port #users duration_of_expt. think_time mode\n", argv[0]);
 		exit(1);
 	}
 
 	portno = atoi(argv[2]);
+	NUM_THREADS = atoi(argv[3]);
+	duration = atof(argv[4]);
+	think_time = atof(argv[5]);
+
+	char* _random = argv[6];
+	if(_random[0] == 'r' && _random[1] == 'a' && _random[2] == 'n' && _random[3] == 'd' && _random[4] == 'o' && _random[5] == 'm' && _random[6] == '\0')
+		isRandom = 1;
+	else if(_random[0] == 'f' && _random[1] == 'i' && _random[2] == 'x' && _random[3] == 'e' && _random[4] == 'd' && _random[5] == '\0')
+		isRandom = 0;
+	else{
+		fprintf(stderr, "last argument can be only \"random\" or \"fixed\"\n");
+		exit(1);
+	}
 
 	/* fill in server address in sockaddr_in datastructure */
 	server = gethostbyname(argv[1]);
@@ -82,8 +111,9 @@ int main(int argc, char *argv[]){
 	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
 	serv_addr.sin_port = htons(portno);
 
+	srand(time(NULL));
+	
 	// create the necessary threads
-	int NUM_THREADS = 10;
 	pthread_t threads[NUM_THREADS];
 	for(int i=0;i<NUM_THREADS;++i){
 		pthread_create(&threads[i], NULL, _main, NULL);	
