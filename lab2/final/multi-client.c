fde8a958dde43eae58884a67a14795bb524510a9
Pradyot Prakash <pp2105@gmail.com>
2016-01-28 23:54:49 +0530

    lab4 and lab5 PS added

--- lab2/final/multi-client.c
+++ lab2/final/multi-client.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <netdb.h> 
 #include <string.h>
 #include <pthread.h>
-#include <unistd.h>
-#include <time.h>
-#include <stdlib.h>
 
 #define PACKETSIZE 512
 
